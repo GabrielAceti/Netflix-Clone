20faef319b5b6b90281e43eade19f13ce288fbdb
GabrielAceti <gabriel.aceti@hotmail.com>
2021-08-19 10:04:00 -0300

    Limitating description and responsive configuration

--- src/components/FeaturedMovie/Index.tsx
+++ src/components/FeaturedMovie/Index.tsx
@@ -9,6 +9,11 @@ export default ({item}: any) => {
         genres.push(item.genres[i].name);
     }
 
+    let description = item.overview;
+    if(description.length > 150){
+        description = description.substring(0,200) + '...'
+    }
+
     return(
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -23,7 +28,7 @@ export default ({item}: any) => {
                         <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1? 's' : ''}</div>
                     </div>
-                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--description">{description}</div>
                     <div className="featured--buttons">
                         <a className="featured--play" href={`/watch/${item.id}`}>► Play</a>
                         <a className="featured--add" href={`/list/add/${item.id}`}>+ My List</a>
